0deb6d2b3805f6b939d168a7ea3fc5f468017d82
ashish <ashishkr.art@gmail.com>
2019-11-01 15:23:32 -0700

    getting count of tickets for last 24 hours.:)

--- main.py
+++ main.py
@@ -10,12 +10,9 @@ import time
 
 #data
 
-
-Category =["Crashing", "Freezing", "Loading", "Bought Currency/Got Kicked Out","Bought Currency/Items Did Not Receive","Bought Currency/Wrong Amount","Charged without Purchase","Missing Items","Game engine not working","Event Issues","Event Questions","Feedback" ,"Game Question","Finished All Levels" ,"Partial Progress Loss","Progress Not saving","Unlocking Next Level","Reset to 1",
+category_list =["Crashing", "Freezing", "Loading", "Bought Currency/Got Kicked Out","Bought Currency/Items Did Not Receive","Bought Currency/Wrong Amount","Charged without Purchase","Missing Items","Game engine not working","Event Issues","Event Questions","Feedback" ,"Game Question","Finished All Levels" ,"Partial Progress Loss","Progress Not saving","Unlocking Next Level","Reset to 1",
 "Multiple Device Sync","Connect to FB","Chat Report","Video Ads","Other"]
 
-
-print( Category)
 
 
 now = int(round(time.time() * 1000))
@@ -24,7 +21,7 @@ yester2= yesterday - 86400000
 
 
 def endpoint():
-    endpoint = url + '/issues' 
+    endpoint = url + 'issues/' 
     return endpoint
 
 
@@ -37,16 +34,17 @@ def api_call(api_key,game,category,start,end):
         'Authorization': b'Basic '+encode,
     }
 
-    cif_type = "dropdown:"
-    category_param = ',"category":{"is_set":true, "is": "%s"}'%category
-    game_param = game":{"is_set":true, "is":"%s"}%game"
+    cif_type = '"dropdown":'
+    category_param = '"category":{"is_set":true, "is": "%s"}'%category
+    game_param = '"game":{"is_set":true, "is":"%s"}'%game
 
     if category is None:
       category_param =""
-    
-    cif = '{"dropdown": {"and": {"game":{"is_set":true, "is":"%s"}%s }}}'%(game,category_param)
-   
-    
+    else:
+      category_param = ',' + category_param
+    # cif = '{"dropdown": {"and": {"game":{"is_set":true, "is":"%s"}%s }}}'%(game,category_param)
+    cif ='{%s {"and": {%s%s}}}'%(cif_type, game_param,category_param)  
+
     params = (
 
 
@@ -56,7 +54,30 @@ def api_call(api_key,game,category,start,end):
     ('created-until', str(end)),
     ('page-size', '100'))
     
-
      
     response = requests.get(url, headers=headers, params=params)
     return response.json()     
+
+def retrieve_count():
+    
+    for each_game in game:
+      print ("Last 24 hours...")
+      response_json = api_call(api_key,each_game,None,yesterday,now)
+      print ("%s : Total %d"%(each_game,response_json['total-hits']))
+      for each_category in category_list:
+          
+          response_json = api_call(api_key,each_game,each_category,yesterday,now)
+          count = response_json['total-hits']
+          if count > 0:
+            print ('%s : %d'%(each_category,response_json['total-hits']))    
+      print('-------------------')
+
+def action():
+	retrieve_count()    
+
+if __name__ == "__main__":
+
+	so = endpoint()
+	print(endpoint)
+	print(url)
+	action()
