24d1c2d43d3a15358d5464256b304d8256d3e73f
ashish <ashishkr.art@gmail.com>
2019-11-07 19:08:26 -0800

    final code cleaning

--- bunny.py
+++ bunny.py
@@ -1,52 +1,79 @@
+#----------------------------------------------------------------------
+#----------------------------------------------------------------------
+#  bunny : responsible for webhooks to CS channels on slack. It calls 
+#  hs_main to get all the relvant data
 
-from secret import ch_public, ch_private, game
+#  Author: Ashish Kumar ( Github: Axeish) 
+
+#  Project entry point : Bunny.py | job
+#----------------------------------------------------------------------
+
+#My Library
+from secret import game
+from hs_main import retrieve_count
+
+#Python Library
 import json
 import requests
 import schedule
 import time
 
-from hs_main import retrieve_count
-
-
+#--------------------------------------------------------------
+#fucntionalities
 
 def compile_slack(data):
-  # code to just use slack formating 
-   message =  "Hello, %s Team \n Daily Summary for %s\n"%(data["game"],data["date"])
-   message = message + "*%s*\n"%data["total"]
-   message = message + "*Category:*\n"
-   for each in data["category"]:
-     message = message  +each + '\n'
-   return message
+  '''
+  code to just use leftover slack formating
+  '''
+
+  total = "Total %d"%(data["total"])
+  message =  "Hello, %s Team \n Daily Summary for %s\n"%(data["game"],data["date"])
+  message = message + "*%s*\n"%total
+  message = message + "*Category:*\n"
+  
+  for each in data["category"]:
+    message = message + each + '\n'
+  return message
 
 
 def webhook_call(data=None):
+  '''
+  This function is just a capsule for the scheduler 
+  '''
+
   for each_game,token in game.items():
     mydata = retrieve_count(each_game)
     message = compile_slack(mydata)
     slack_data = {'text': message}
     
-    response = requests.post(
-      token, data=json.dumps(slack_data),
-    headers={'Content-Type': 'application/json'}
-  )
-  if response.status_code != 200:
-    raise ValueError(
-        'Request to slack returned an error %s, the response is:\n%s'
-        % (response.status_code, response.text)
-    )
 
+    try:
+
+      response = requests.post(
+        token, data = json.dumps(slack_data),
+        headers = {'Content-Type': 'application/json'})
+
+      print('-------successfully posed to ------%s'%each_game)
+    
+    except requests.exceptions.RequestException as e:  
+        print (e)
+        sys.exit(1)  
+  
 
 
 def job(t):
-    webhook_call()
+  '''
+  # The main Scheduler function start from here . This is also the Entry point for this entire project
+  '''  
+  webhook_call()
 
-schedule.every().day.at("17:51").do(job,'')
+
+schedule.every().day.at("5:30").do(job,'')
 
 while True:
     schedule.run_pending()
     time.sleep(60) # wait one minute
+
 '''
-
-
 webhook_call()
 '''
--- hs_main.py
+++ hs_main.py
@@ -1,5 +1,16 @@
-from secret import  api_key, url, domain, game
-from issue_format import display_request
+#----------------------------------------------------------------------
+#----------------------------------------------------------------------
+#  hs_main : Mostly responsible for making API calls to HelpShift and 
+#  Obtaining/Creating relevant data to be used by Slack
+
+#  Author: Ashish Kumar ( Github: Axeish) 
+
+#  Project entry point : Bunny.py | job
+#----------------------------------------------------------------------
+
+#My Library
+from secret import  api_key, url, domain, game, category_list
+
 #Python Library
 from datetime import datetime, time, timedelta
 import requests
@@ -9,101 +20,108 @@ import sys
 
 
 
-#data
-
-category_list =["_stability_","Crashing", "Freezing", "Loading","_purchase_","Bought Currency/Got Kicked Out","Bought Currency/Items Did Not Receive","Bought Currency/Wrong Amount","Charged without Purchase","_game issues_", "Missing Items","Game engine not working","Event Issues","Event Questions","Finished All Levels" ,"Partial Progress Loss","Progress Not saving","Unlocking Next Level","Reset to 1",
-"_other_","Feedback" ,"Game Question", "Multiple Device Sync","Connect to FB","Chat Report","Video Ads","Other"]
-
-
-
-#now = int(round(time.time() * 1000))
-#yesterday = now - 86400000
-#yester2= yesterday - 86400000
-
-
 def midnight():
     midnight = datetime.combine(datetime.today(), time.min)
     today = int(midnight.timestamp() *1000)
     return today
 
+
 today = midnight()
 yesterday = today - 86400000    
-yester2= yesterday - 86400000
+yester2= yesterday - 86400000 
+
 
 def endpoint():
     endpoint = url + 'issues/' 
     return endpoint
 
+def slack_bolder(data):
+    return "*" + data + "*"
 
-def api_call(api_key,game,category,start,end):
+#--------------------------------------------------------------
+#fucntionalities
 
+
+def api_call_request(game,category,start,end):
+    '''
+    defines relevant headers and params for api call and then 
+    makes that api call
+    '''
+    
     url = endpoint()    
     encode = base64.b64encode(api_key.encode("UTF-8"))
+    cif_type = '"dropdown":'
+    category_param = '"category":{"is_set":true, "is": "%s"}'%category
+    game_param = '"game":{"is_set":true, "is":"%s"}'%game
+
+    # formating different types of api call dynamically. 
+    if category is None:
+      category_param = ""
+
+    elif category[0] == "_":
+      return None
+
+    else:
+      category_param = ',' + category_param
+
+    cif = '{%s {"and": {%s%s}}}'%(cif_type, game_param,category_param)  
+
     headers = {
         'Accept': 'application/json',
         'Authorization': b'Basic '+encode,
     }
 
-    cif_type = '"dropdown":'
-    category_param = '"category":{"is_set":true, "is": "%s"}'%category
-    game_param = '"game":{"is_set":true, "is":"%s"}'%game
-
-    if category is None:
-      category_param =""
-    elif category[0] == "_":
-      return None    
-    else:
-      category_param = ',' + category_param
-    # cif = '{"dropdown": {"and": {"game":{"is_set":true, "is":"%s"}%s }}}'%(game,category_param)
-    cif ='{%s {"and": {%s%s}}}'%(cif_type, game_param,category_param)  
-
     params = (
-
-
     ('created-since', str(start)),
     ('state', 'new,resolved,new-for-agent,agent-replied,waiting-for-agent,pending-reassignment'),
     ('custom_fields', cif),
     ('created-until', str(end)),
     ('page-size', '100'))
     
-     
-    response = requests.get(url, headers=headers, params=params)
-    return response.json()   
+    try:
 
+        response = requests.get(url, headers=headers, params=params)
+    except requests.exceptions.RequestException as e:  
+        print (e)
+        sys.exit(1)    
+    return response.json()
 
 
 def retrieve_count(my_game = None):
-    my_game_list = []   
+    '''
+    Game_data is a dictionary that will be  get filled after each api call. 
+    returning game_data
+
+    '''
     game_data = {}
-    game_data["game"]= my_game
-    game_data["date"]= (datetime.today()-timedelta(1)).strftime("%B %d, %Y")
-    if my_game:
-    	my_game_list.append(my_game)
-   
+    game_data["game"] = my_game
+    game_data["date"] = (datetime.today() - timedelta(1)).strftime("%B %d, %Y")
+    
 
-    for each_game in my_game_list:
-      response_json = api_call(api_key,each_game,None,yesterday,today)
-      total = " Total %d"%(response_json['total-hits'])
-      game_data["total"]=total
-      game_data["category"] = []	
-      for each_category in category_list:
+    response_json = api_call_request(my_game,None,yesterday,today)
+    
+    game_data["total"] = response_json['total-hits']
+    
+    game_data["category"] = []
 
-          highlight = ""
-          response_json = api_call(api_key,each_game,each_category,yesterday,today)
-          if response_json == None:
-            cat_data = each_category
-            game_data["category"].append(cat_data)
+    for each_category in category_list:
+          highlight = ""   #highlight data for anything more than 10 in count
+          response_json = api_call_request(my_game,each_category,yesterday,today)
+          
+          if response_json == None:             
+            game_data["category"].append(each_category)
             
           else:  
             count = response_json['total-hits']
             if count > 0:
               if count >10:
-                each_category = "*"+each_category+"*"
+                each_category = slack_bolder(each_category)
                 highlight="*"
               cat_data = '>%s : %d%s'%(each_category,response_json['total-hits'],highlight)
               game_data["category"].append(cat_data)    
-      print('-------------------')
-      return game_data 
+
+    
+    return game_data 
 
 
 
